e58379cd679279e0a3ccfd345fa3ba781a5142e0
Pal Kerecsenyi <pal@p.kerecs.com>
2014-02-21 14:16:13 +0100

    init: set up base invenio-vcs repository, without important code

---
+++ invenio_vcs/proxies.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Proxy for current previewer."""
+
+from flask import current_app
+from werkzeug.local import LocalProxy
+
+current_vcs = LocalProxy(lambda: current_app.extensions["invenio-vcs"])
