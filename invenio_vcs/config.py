49c94ad7b1829ade79f898da850cf0d9be54d171
Pal Kerecsenyi <pal@p.kerecs.com>
2026-02-03 16:24:31 +0100

    feat: provider layer

---
+++ invenio_vcs/config.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""You can use these options to configure the Invenio-VCS module.
+
+Other than ``VCS_PROVIDERS``, they are all optional and configured with reasonable defaults.
+"""
+
+from typing import TYPE_CHECKING
+
+from flask import current_app
+
+if TYPE_CHECKING:
+    from invenio_vcs.providers import RepositoryServiceProviderFactory
+
+VCS_PROVIDERS = []
+"""The list of RepositoryProviderFactory instances.
+
+These will be visible to the user in their settings and they will be able to sync repositories
+from all of them. Multiple instances of different providers as well as of the same provider
+can be combined in this list, but each provider must have a unique ``id`` and ``credentials_key``.
+
+>>> from invenio_vcs.contrib.github import GitHubProviderFactory
+
+>>> _vcs_github = GitHubProviderFactory(
+...     base_url="https://github.com",
+...     webhook_receiver_url="https://example.com/api/hooks/receivers/github/events/?access_token={token}",
+... )
+
+>>> VCS_PROVIDERS = [_vcs_github]
+
+For more details, refer to the usage documentation.
+"""
+
+VCS_PROVIDER_CONFIG_DICT = {}
+"""An optional dictionary of configuration overrides for RepositoryProviderFactory instances.
+
+This makes it possible to specify configuration values via environment variables rather than as
+class constructor parameters, allowing for easier secret setting.
+"""
+
+VCS_RELEASE_CLASS = "invenio_vcs.service:VCSRelease"
+"""VCSRelease class to be used for release handling."""
+
+VCS_TEMPLATE_INDEX = "invenio_vcs/settings/index.html"
+"""Repositories list template."""
+
+VCS_TEMPLATE_INDEX_ITEM = "invenio_vcs/settings/index_item.html"
+"""Repositories list item template."""
+
+VCS_TEMPLATE_VIEW = "invenio_vcs/settings/view.html"
+"""Repository detail view template."""
+
+VCS_TEMPLATE_REPO_SWITCH = "invenio_vcs/settings/repo_switch.html"
+"""Repository toggle switch template."""
+
+VCS_TEMPLATE_RELEASE_ITEM = "invenio_vcs/settings/release_item.html"
+"""Release item template, used in the 'view' page."""
+
+VCS_ERROR_HANDLERS = None
+"""Definition of the way specific exceptions are handled."""
+
+VCS_MAX_CONTRIBUTORS_NUMBER = 30
+"""Max number of contributors of a release to be retrieved from vcs."""
+
+VCS_CITATION_FILE = None
+"""Citation file name."""
+
+VCS_CITATION_METADATA_SCHEMA = None
+"""Citation metadata schema."""
+
+VCS_ZIPBALL_TIMEOUT = 300
+"""Timeout for the zipball download, in seconds."""
+
+VCS_SYNC_BATCH_SIZE = 20
+"""Number of repositories to be processed in a single batch when syncing hooks and users.
+
+If the user has more than 20 repositories, multiple tasks will be created,
+syncing them in parallel. Thereby the sync process should finish in a timely
+manner and we avoid timeouts on platforms like Zenodo.
+
+Decrease this value if you experience task timeouts.
+"""
+
+
+def get_provider_list(app=current_app) -> list["RepositoryServiceProviderFactory"]:
+    """Get a list of configured VCS provider factories."""
+    return app.config["VCS_PROVIDERS"]
+
+
+def get_provider_by_id(id: str) -> "RepositoryServiceProviderFactory":
+    """Get a specific VCS provider by its registered ID."""
+    providers = get_provider_list()
+    for provider in providers:
+        if id == provider.id:
+            return provider
+    raise Exception(f"VCS provider with ID {id} not registered")
+
+
+def get_provider_config_override(id: str, app=current_app) -> dict:
+    """Get the config override dict for a provider by ID, or an empty dictionary by default."""
+    return app.config["VCS_PROVIDER_CONFIG_DICT"].get(id, {})
---
+++ invenio_vcs/generic_models.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+"""
+Generic dataclass models to represent the bare minimum necessary data from VCS providers.
+
+These are essentially the "lowest common factor" of
+the otherwise large, complex, and heterogenous responses returned by APIs.
+
+These are used by higher-level calls to have a common set of data to
+operate on. Provider implementations are responsible for converting API
+responses into these generic classes.
+"""
+
+from __future__ import annotations
+
+from dataclasses import asdict, dataclass
+from datetime import datetime
+from enum import Enum
+
+from invenio_vcs.models import Repository
+
+
+@dataclass
+class GenericWebhook:
+    """Generic webhook representation."""
+
+    id: str
+    repository_id: str
+    url: str
+
+
+@dataclass
+class GenericRepository:
+    """Generic repository representation.
+
+    Some fields might be None if they are optional and unspecified, or if the repository
+    is returned via a list query rather than a get query.
+    """
+
+    id: str
+    full_name: str
+    default_branch: str
+    description: str | None = None
+    license_spdx: str | None = None
+
+    @staticmethod
+    def from_model(model: Repository):
+        """Create a GenericRepository from a Repository model."""
+        return GenericRepository(
+            id=model.provider_id,
+            full_name=model.full_name,
+            default_branch=model.default_branch,
+            description=model.description,
+        )
+
+    def update_model(self, model: Repository):
+        """Update a Repository model with this generic repository's data."""
+        for key, value in asdict(self).items():
+            if key in ["id", "license_spdx"]:
+                continue
+
+            db_value = getattr(model, key)
+            if db_value != value:
+                setattr(model, key, value)
+
+
+@dataclass
+class GenericRelease:
+    """Generic release representation."""
+
+    id: str
+    repository_id: str
+    tag_name: str
+    created_at: datetime
+    name: str | None = None
+    body: str | None = None
+    tarball_url: str | None = None
+    zipball_url: str | None = None
+    published_at: datetime | None = None
+    """Releases may be published at a different time than when they're created.
+
+    For example, the publication to a package repository (e.g. NPM) may have taken place
+    a few minutes before the maintainers published the release on the VCS. The date may
+    even be in the future if a release is pre-scheduled (quite common on GitLab).
+    """
+
+
+@dataclass
+class GenericUser:
+    """Generic representation of a user.
+
+    This refers to a (usually) human end-user with credentials to log into the VCS,
+    but it can also be a bot account. It cannot however be an organisation/group/etc.
+    They can typically create repositories and become members of organisations.
+    They have a full account identity and are recognised as users by the VCS.
+    """
+
+    id: str
+    username: str
+    display_name: str | None = None
+
+
+class GenericOwnerType(Enum):
+    """Types of repository owners."""
+
+    USER = 1
+    """This corresponds to the type of entity represented by GenericUser."""
+
+    ORGANIZATION = 2
+    """A non-user entity that users can be members of and can thereby act on its behalf subject to permissions."""
+
+
+@dataclass
+class GenericOwner:
+    """Generic repository owner representation.
+
+    This is an entity that owns a repository. For example, on GitHub this is either an organisation
+    or a user, while on GitLab this is a group/sub-group or a user.
+    """
+
+    id: str
+    path_name: str
+    type: GenericOwnerType
+    display_name: str | None = None
+
+
+@dataclass
+class GenericContributor:
+    """Generic contributor representation.
+
+    Contributors might be users, but they don't have to be. The low-level protocol used by the VCS
+    (usually Git) tracks who makes changes to the code, typically separately from the user/accounts
+    system of the VCS itself.
+    For example, when importing a Git repository from one VCS to another (e.g. from self-hosted GitLab
+    to gitlab.com), the contributors might not exist as users on the destination VCS. Despite this,
+    they are still reported as contributors.
+    """
+
+    id: str
+    username: str
+    company: str | None = None
+    contributions_count: int | None = None
+    display_name: str | None = None
