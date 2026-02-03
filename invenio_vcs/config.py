84a8d85603171904cb8707434a66014eebaaf629
Pal Kerecsenyi <pal@p.kerecs.com>
2026-02-03 16:24:31 +0100

    feat: provider layer

--- invenio_vcs/config.py
+++ invenio_vcs/config.py
@@ -23,6 +23,24 @@ VCS_PROVIDERS = []
 These will be visible to the user in their settings and they will be able to sync repositories
 from all of them. Multiple instances of different providers as well as of the same provider
 can be combined in this list, but each provider must have a unique ``id`` and ``credentials_key``.
+
+>>> from invenio_vcs.contrib.github import GitHubProviderFactory
+
+>>> _vcs_github = GitHubProviderFactory(
+...     base_url="https://github.com",
+...     webhook_receiver_url="https://example.com/api/hooks/receivers/github/events/?access_token={token}",
+... )
+
+>>> VCS_PROVIDERS = [_vcs_github]
+
+For more details, refer to the usage documentation.
+"""
+
+VCS_PROVIDER_CONFIG_DICT = {}
+"""An optional dictionary of configuration overrides for RepositoryProviderFactory instances.
+
+This makes it possible to specify configuration values via environment variables rather than as
+class constructor parameters, allowing for easier secret setting.
 """
 
 VCS_RELEASE_CLASS = "invenio_vcs.service:VCSRelease"
@@ -62,3 +80,8 @@ def get_provider_by_id(id: str) -> "RepositoryServiceProviderFactory":
         if id == provider.id:
             return provider
     raise Exception(f"VCS provider with ID {id} not registered")
+
+
+def get_provider_config_override(id: str, app=current_app) -> dict:
+    """Get the config override dict for a provider by ID, or an empty dictionary by default."""
+    return app.config["VCS_PROVIDER_CONFIG_DICT"].get(id, {})
